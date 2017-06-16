af3cbc665742fdb80a77b459b322ddfaf64e364d
Echelan <andres_movilla1103@hotmail.com>
2017-06-16 17:24:21 -0500

    copying and adapting sudoku assets

---
+++ Project/main.lua
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+display.setStatusBar( display.HiddenStatusBar )
+system.setIdleTimer( false )
+
+	
+local y = display.contentHeight - 20
+local fullText = "Developed by Andres Movilla"
+local rightsText = display.newText(fullText,display.contentCenterX,y,native.systemFont,15)
+rightsText:setFillColor(0.3,0.3,0.3)
+
+local menu=require("menu")
+menu.start()
