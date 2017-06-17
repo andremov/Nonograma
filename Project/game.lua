e6bb9989874ae66698df6bd9b6d7ad693646289d
Echelan <andres_movilla1103@hotmail.com>
2017-06-16 21:00:50 -0500

    play progress
    almost done, missing chunk counts

--- Project/game.lua
+++ Project/game.lua
@@ -5,276 +5,334 @@
 -----------------------------------------------------------------------------------------
 module(..., package.seeall)
 
-local difficulty = 6
-
+local dimension -- BETWEEN 5 AND 10
+local preStartMenuGroup
+local boardGroup
+local board
+local solution
+local attempt
+local isPlaying
 
 function init()
-    Build()
+    -- Build()
+	initValues()
+	initPreUI()
+	initBoard()
 end
 
+function initValues()
+	dimension = 7
+	isPlaying = false
+end
 
-function Build()
-	tsize=25
-	xinicial=5
-	yinicial=40
-	tiles={}
-	grid={}
-	texts={}
-	tapped={}
-	for x=1,difficulty+3 do
-		tiles[x]={}
-		tapped[x]={}
-		grid[x]={}
-		texts[x]={}
-		for y=1,difficulty+3 do
-		
-			local curtile=display.newRect(10,10,tsize,tsize)
-			curtile.x=xinicial+((x-1)*(tsize+1))
-			curtile.y=yinicial+((y-1)*(tsize+1))
+function initPreUI()
+	preStartMenuGroup = display.newGroup()
+
+	local midW = display.contentCenterX
+
+	local startBtn=display.newRect(0,0,210,40)
+	startBtn.x=midW
+	startBtn.y=display.contentHeight-70
+	startBtn:setFillColor(0.9,0.9,0.9)
+	startBtn:addEventListener("tap",newPuzzle)
+	preStartMenuGroup:insert(startBtn)
+	
+	local startText=display.newText("Start",0,0,native.systemFont,30)
+	startText.x=midW
+	startText.y=startBtn.y
+	startText:setTextColor(0,0,0)
+	preStartMenuGroup:insert(startText)
+
+
+	local reduceBtn=display.newRect(0,0,100,40)
+	reduceBtn.x=midW-55
+	reduceBtn.y=display.contentHeight-130
+	reduceBtn:setFillColor(0.8,0.5,0.5)
+	reduceBtn:addEventListener("tap",reduceDimension)
+	preStartMenuGroup:insert(reduceBtn)
+	
+	local reduceText=display.newText("Reduce",0,0,native.systemFont,25)
+	reduceText.x=reduceBtn.x
+	reduceText.y=reduceBtn.y
+	reduceText:setTextColor(0,0,0)
+	preStartMenuGroup:insert(reduceText)
+
+	local addBtn=display.newRect(0,0,100,40)
+	addBtn.x=midW+55
+	addBtn.y=display.contentHeight-130
+	addBtn:setFillColor(0.5,0.8,0.5)
+	addBtn:addEventListener("tap",addDimension)
+	preStartMenuGroup:insert(addBtn)
+	
+	local addText=display.newText("Add",0,0,native.systemFont,25)
+	addText.x=addBtn.x
+	addText.y=reduceBtn.y
+	addText:setTextColor(0,0,0)
+	preStartMenuGroup:insert(addText)
+
+	local backBtn=display.newRect(0,0,120,100)
+	backBtn.x=-20
+	backBtn.y=display.contentHeight-100
+	backBtn:setFillColor(0.4,0.4,0.4)
+	backBtn:addEventListener("tap",menuCall)
+	preStartMenuGroup:insert(backBtn)
+	
+	local backText=display.newText("Back",0,0,native.systemFont,15)
+	backText.x=10
+	backText.y=backBtn.y
+	backText:setTextColor(0.7,0.7,0.7)
+	preStartMenuGroup:insert(backText)
+
+end
+
+function newTile(x,y)
+
+	local tileSize = 23
+
+	local tile = display.newRect(0,0,tileSize,tileSize)
+	tile.x=15	+((x-1)*(tileSize+1))
+	tile.y=20	+((y-1)*(tileSize+1))
+	tile.isTapped = false
+	tile.isSolution = false
+	tile.isGuide = false
+
+	if y>3 then
+		tile.y=tile.y+2
+	end
+	
+	if x>3 then
+		tile.x=tile.x+2
+	end
+
+	tile.label = display.newText("",0,0,native.systemFont,20)
+	tile.label.x = tile.x
+	tile.label.y = tile.y
+	tile.label:setFillColor(0,0,0)
+
+	tile.setText = function(self, message)
+		self.label.text = message
+		self.isGuide = true
+	end
+
+	tile.setSolution = function(self, value)
+		self.isSolution = value
+	end
+
+	tile.reset = function(self)
+		self.isSolution = false
+		self.isTapped = false
+		self.isGuide = false
+		self.label.text = ""
+	end
+
+	tile.dispose = function(self)
+		display.remove(self.label)
+		display.remove(self)
+	end
+
+	tile.tap = function(self)
+		if (isPlaying) then
+			if not (self.isGuide) then
+				self.isTapped = true
+				if (self.isSolution) then
+					self:setFillColor(0.5,0.8,0.5)
+				else
+					self:setFillColor(0.8,0.5,0.5)
+				end
+			end
+		end
+	end
+	tile:addEventListener("tap",tile)
+
+	return tile
+end
+
+function initBoard()
+	display.remove(boardGroup)
+	boardGroup = display.newGroup()
+
+	board = {}
+	for x=1,dimension+3 do
+		board[x] = {}
+		for y=1,dimension+3 do
 			
-			local curtext=display.newText(0,0,0,native.systemFont,20)
-			curtext.x=curtile.x
-			curtext.y=curtile.y
-			curtext:setFillColor(0,0,0)
+			local curtile=newTile(x,y)
 			
 			if x<4 and y<4 then
-				display.remove(curtile)
-				curtile=nil
-				display.remove(curtext)
-				curtext=nil
+				curtile:dispose()
+			else
+				board[x][y] = curtile
+				boardGroup:insert(board[x][y])
 			end
-			
-			if y>3 then
-				curtile.y=curtile.y+2
-			end
-			
-			if x>3 then
-				curtile.x=curtile.x+2
-			end
-			
-			tiles[x][y]=curtile
-			
-			if y>3 and x>3 then
-				display.remove(curtext)
-				curtext=nil
-				
-				function Wrapper()
-					Action(x,y)
-				end
-				
-				tiles[x][y]:addEventListener("tap",Wrapper)
-			end
-			
-			texts[x][y]=curtext
-			
 		end
 	end
-	New()
 end
 
-function New()
-	for x=1,difficulty+3 do 
-		for y=1,difficulty+3 do
-			grid[x][y]=0
-			if (texts[x][y]) then
-				if (texts[x][y].text) then
-					texts[x][y].text="0"
-				end
+function deleteBoard()
+	for x = 1, table.maxn(board) do
+		for y = 1, table.maxn(board) do
+			if (board[x][y]) then
+				board[x][y]:dispose()
 			end
 		end
 	end
-	
-	for x=4,difficulty+3 do
-		for y=4,difficulty+3 do
-			grid[x][y]=math.random(0,1)
-			tiles[x][y]:setFillColor(1,1,1)
-			-- if grid[x][y]==1 then
-				tapped[x][y]=false
-				-- tiles[x][y]:setFillColor(0.5,0.5,0.5)
-			-- else
-				-- tapped[x][y]=true
-			-- end
-		end
-	end
-	
-	local changed=true
-	while changed==true do
-		print "CHECK!"
-		changed=false
-		
-		for y=4,difficulty+3 do
-			local xcount=0
-			for x=4,difficulty+3 do
-				if grid[x][y]==1 and grid[x-1][y]==0 and xcount<3 then
-					xcount=xcount+1
-				elseif grid[x][y]==1 and grid[x-1][y]==0 then
-					grid[x][y]=0
-					tiles[x][y]:setFillColor(1,1,1)
-					print "DELETED!"
-					changed=true
-				end
-			end
-			print ("Y-"..y-3 .."  count-"..xcount)
-		end
-		print "--"
-		for x=4,difficulty+3 do
-			local ycount=0
-			for y=4,difficulty+3 do
-				if grid[x][y]==1 and grid[x][y-1]==0 and ycount<3 then
-					ycount=ycount+1
-				elseif grid[x][y]==1 and grid[x][y-1]==0 then
-					grid[x][y]=0
-					tiles[x][y]:setFillColor(1,1,1)
-					print "DELETED!"
-					changed=true
-				end
-			end
-			print ("X-"..x-3 .."  count-"..ycount)
-		end
-	end
+	display.remove(boardGroup)
+end
 
-	-- print "CHECK!"
-	-- changed=false
-	
-	for y=4,difficulty+3 do
-		local group=0
-		for x=difficulty+3,3,-1 do
-			if grid[x][y]==1 then
-				group=group+1
-			else
-				local set=false
-				local cont=3
-				while (set==false and cont>0) do
-					
-					if (texts[cont][y].text=="0") then
-						texts[cont][y].text=group
-						set=true
-					else
-						cont=cont-1
-					end
-				end
-				group=0
-			end
-		end
-		-- print ("Y-"..y-3 .."  count-"..xcount)
-	end
-	-- print "--"
-	for x=4,difficulty+3 do
-		local group=0
-		for y=difficulty+3,3,-1 do
-			if grid[x][y]==1 then
-				group=group+1
-			else
-				local set=false
-				local cont=3
-				while (set==false and cont>0) do
-					if (texts[x][cont].text=="0") then
-						texts[x][cont].text=group
-						set=true
-					else
-						cont=cont-1
-					end
-				end
-				group=0
-			end
-		end
-		-- print ("X-"..x-3 .."  count-"..ycount)
-	end
+function deleteEverything()
+	deleteBoard()
+	display.remove(preStartMenuGroup)
+end
 
-	for x=1,difficulty+3 do
-		for y=1,difficulty+3 do
-			if (texts[x][y]) then
-				if (texts[x][y].text=="0") then
-					texts[x][y].isVisible=false
+function menuCall()
+	deleteEverything()
+	local menu = require("menu")
+	menu.start()
+end
+
+function addDimension()
+	dimension = dimension+1
+	if (dimension > 10) then
+		dimension = 10
+	end
+	initBoard()
+end
+
+function reduceDimension()
+	dimension = dimension-1
+	if (dimension < 5) then
+		dimension = 5
+	end
+	initBoard()
+end
+
+function resetBoard()
+	for x = 1, table.maxn(board) do
+		for y = 1, table.maxn(board) do
+			if (board[x][y]) then
+				board[x][y]:reset()
+			end
+		end
+	end
+end
+
+function newPuzzle()
+	resetBoard()
+	display.remove(preStartMenuGroup)
+	isPlaying = true
+
+	grid = {}
+
+	for x=1, dimension do 
+		grid[x] = {}
+		for y=1, dimension do
+			grid[x][y]=(math.random(0,1)==1)
+		end
+	end
+	
+	-- REMOVE EXTRA CHUNKS
+	for x = 1, dimension do
+		local chunks = 0
+		local inChunk = false
+		for y = 1, dimension do
+			if (grid[x][y] ~= inChunk) then
+				if (not inChunk and chunks < 3) then
+					chunks = chunks+1
+					inChunk = true
+				elseif (not inChunk) then
+					grid[x][y] = not grid[x][y]
 				else
-					texts[x][y].isVisible=true
+					inChunk = false
 				end
 			end
 		end
 	end
 	
-	for y=4,difficulty+3 do
-		local xdone=true
-		for x=4,difficulty+3 do
-			if (tapped[x][y]==false and grid[x][y]==1) then
-				xdone=false
-			end
-		end
-		if xdone==true then
-			for x=4,difficulty+3 do
-				if (tapped[x][y]==false) then
-					tiles[x][y]:setFillColor(0.2,0.2,0.8)
+	for y = 1, dimension do
+		local chunks = 0
+		local inChunk = false
+		for x = 1, dimension do
+			if (grid[x][y] ~= inChunk) then
+				if (not inChunk and chunks < 3) then
+					chunks = chunks+1
+					inChunk = true
+				elseif (not inChunk) then
+					grid[x][y] = not grid[x][y]
+				else
+					inChunk = false
 				end
 			end
 		end
 	end
-	
-	for x=4,difficulty+3 do
-		local ydone=true
-		for y=4,difficulty+3 do
-			if (tapped[x][y]==false and grid[x][y]==1) then
-				ydone=false
-			end
-		end
-		if ydone==true then
-			for y=4,difficulty+3 do
-				if (tapped[x][y]==false) then
-					tiles[x][y]:setFillColor(0.2,0.2,0.8)
-				end
-			end
-		end
-	end 
-end
 
-function Action(x,y)
-	if tapped[x][y]==false then
-		-- print (x..","..y)
-		if grid[x][y]==1 then
-			tapped[x][y]=true
-			tiles[x][y]:setFillColor(0.2,0.8,0.2)
-		elseif grid[x][y]==0 then
-			tapped[x][y]=true
-			tiles[x][y]:setFillColor(0.8,0.2,0.2)
+	-- TELL TILES
+	for x = 1, dimension do
+		for y = 1, dimension do
+			board[x+3][y+3]:setSolution(grid[x][y])
 		end
-		
-		local xdone=true
-		for ax=4,difficulty+3 do
-			if (tapped[ax][y]==false and grid[ax][y]==1) then
-				xdone=false
-			end
-		end
-		if xdone==true then
-			for ax=4,difficulty+3 do
-				if (tapped[ax][y]==false) then
-					tapped[ax][y]=true
-					tiles[ax][y]:setFillColor(0.2,0.2,0.8)
-				end
-			end
-		end
-		
-		local ydone=true
-		for ay=4,difficulty+3 do
-			if (tapped[x][ay]==false and grid[x][ay]==1) then
-				ydone=false
-			end
-		end
-		if ydone==true then
-			for ay=4,difficulty+3 do
-				if (tapped[x][ay]==false) then
-					tapped[x][ay]=true
-					tiles[x][ay]:setFillColor(0.2,0.2,0.8)
-				end
-			end
-		end
-		
-		Check()
 	end
+
+	-- DO CHUNK COUNT
+	for x = 1, dimension do
+		local chunkCount = {0,0,0,0}
+		local thisChunk = 1
+		local inChunk = false
+		for y = 1, dimension do
+			if (grid[x][y]) then
+				inChunk = true
+				chunkCount[thisChunk] = chunkCount[thisChunk] + 1
+			else
+				if (inChunk) then
+					inChunk = not inChunk
+					thisChunk = thisChunk+1
+				end
+			end
+		end
+
+		if (chunkCount[thisChunk] == 4) then
+			thisChunk = thisChunk - 1
+		end
+
+		for i = 3-(thisChunk-1), 1, -1 do
+			if (chunkCount[4-i] ~= 0) then
+				board[x+3][i]:setText(chunkCount[4-i])
+			end
+		end
+	end
+
+	for y = 1, dimension do
+		local chunkCount = {0,0,0,0}
+		local thisChunk = 1
+		local inChunk = false
+		for x = 1, dimension do
+			if (grid[x][y]) then
+				inChunk = true
+				chunkCount[thisChunk] = chunkCount[thisChunk] + 1
+			else
+				if (inChunk) then
+					inChunk = not inChunk
+					thisChunk = thisChunk+1
+				end
+			end
+		end
+
+		if (chunkCount[thisChunk] == 4) then
+			thisChunk = thisChunk - 1
+		end
+
+		for i = 3-(thisChunk-1), 1, -1 do
+			if (chunkCount[4-i] ~= 0) then
+				board[i][y+3]:setText(chunkCount[4-i])
+			end
+		end
+	end
+
 end
 
 function Check()
 	local finish=true
-	for x=4,difficulty+3 do
-		for y=4,difficulty+3 do
+	for x=4,dimension+3 do
+		for y=4,dimension+3 do
 			if tapped[x][y]==false and grid[x][y]==1 then
 				finish=false
 			end
